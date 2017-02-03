14dd38b1b473d359e61a7157bf309f90717d5544
alexFrankfurt <alex_frankfurt@mail.ru>
2017-02-03 15:12:25 +0300

    query database for data

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,20 +9,25 @@ import play.api.libs.ws.WSClient
 import play.api.mvc.{Action, Controller}
 import akka.pattern.ask
 import akka.util.Timeout
+import models.db.Tables
+import services.db.DBService
+import utils.db.TetraoPostgresDriver.api._
+
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 
 
-class Application @Inject() (ws: WSClient, system: ActorSystem) extends Controller{
+class Application @Inject() (ws: WSClient, system: ActorSystem, database: DBService) extends Controller{
 
   implicit val timeout = Timeout(5 seconds)
 
   private val requestActor = system.actorOf(RequestActor.props(ws))
 
-  def index() = Action.async {
-    (requestActor ? GetData).mapTo[String].map { resp =>
-      Ok(resp)
-    }
+  def index() = Action {
+    val rows = database.run{
+      val testData = List("Oha")
+      Tables.Properties.filter(_.keywords @> testData).result}
+    val res = rows.map {row => row.keywords}
+    Ok(res.toString())
   }
-
 }
